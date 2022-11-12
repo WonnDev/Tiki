1b5d4402a5f0942b98bbd8f03c06d1f9c34ffdea
Wonn <wonndev@gmail.com>
2022-11-12 18:26:47 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-html {
-    font-family: Roboto,Helvetica,Arial,sans-serif;
-    font-size: 14px;
-}
 /***** Cart *****/
+/*btn checkall*/
+function myFunction() {
+    var checkBox = document.getElementById("CheckAll");
+    var checkBoxChild1 = document.getElementById("check-box-1");
+    var checkBoxChild2 = document.getElementById("check-box-2");
+    if (checkBox.checked == true){
+      checkBoxChild1.checked = true;
+      checkBoxChild2.checked = true;
+    }
+  }

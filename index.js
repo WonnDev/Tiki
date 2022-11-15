c9b67721aa24a7e92bf3ee37a1e12953d2f31f2e
Wonn <wonndev@gmail.com>
2022-11-15 18:49:12 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 /***** Cart *****/
-/*btn checkall*/
-function myFunction() {
-    var checkBox = document.getElementById("CheckAll");
-    var checkBoxChild1 = document.getElementById("check-box-1");
-    var checkBoxChild2 = document.getElementById("check-box-2");
-    if (checkBox.checked == true){
-      checkBoxChild1.checked = true;
-      checkBoxChild2.checked = true;
+
+  /*PathDown*/
+  function showContentPathDown () {
+    var x = document.getElementById("content-path-down");
+
+
+    if(x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none" ;
     }
   }
+  /*btn checkall*/
+  function btnCheckAll() {
+    var checkBox = document.getElementById("Checked");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }
+
+  function checkItem() {
+    var checkBoxItem = document.getElementsByClassName("itemchecked"),
+        elementItem = document.getElementsByClassName("item-of-checked");
+        if (checkBoxItem.checked == true) {
+          for (var i = 0; i < elementItem.length; i++) {
+              elementItem[i].style.display = "block";
+              // y[i].style.display = "none";
+          } 
+        } else {
+          checkBoxItem[i].style.display = "none";
+          // y[i].style.display = "block";
+    }
+}
+
+
+  
